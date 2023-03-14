260753cdd022d6b42c9d06672a340352bab84445
tomarpooja <tomarpooja005108@gmail.com>
2023-03-14 19:08:10 +0530

    Initial commit

---
+++ src/SwitchStringExample.java
@@ -0,0 +1,17 @@
+class Animal{
+    void eat(){System.out.println("eating...");}
+}
+class Dog extends Animal{
+    void bark(){System.out.println("barking...");}
+}
+class TestInheritance{
+    public static void main(String[] args) {
+
+    }
+
+    static {
+        Dog d=new Dog();
+        d.bark();
+        d.eat();
+    }
+}
